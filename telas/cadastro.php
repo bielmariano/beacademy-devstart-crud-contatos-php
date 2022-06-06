ba43ca821b5a7f3a9574ae3a332c1b3a0fe5b1e2
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-06-06 14:47:28 -0300

    fist commit

---
+++ telas/cadastro.php
@@ -0,0 +1,11 @@
+<h1>Cadastro</h1>
+<hr>
+<form action="" method="post">
+  <input class="form-control mb-3" name="nome" type="text" placeholder="nome">
+
+  <input class="form-control mb-3" name="email" type="text" placeholder="email">
+  
+  <input class="form-control mb-3" name="telefone" type="text" placeholder="telefone">
+
+  <button class="btn btn-primary">Enviar</button>
+</form>
---
+++ telas/editar.php
@@ -0,0 +1,11 @@
+<h1>Editar Contato</h1>
+<hr>
+<form action="" method="post">
+  <input value="<?php echo $dados[0]; ?>" class="form-control mb-3" name="nome" type="text" placeholder="nome">
+
+  <input value="<?php echo $dados[1]; ?>" class="form-control mb-3" name="email" type="text" placeholder="email">
+  
+  <input value="<?php echo $dados[2]; ?>" class="form-control mb-3" name="telefone" type="text" placeholder="telefone">
+
+  <button class="btn btn-primary">Enviar</button>
+</form>
